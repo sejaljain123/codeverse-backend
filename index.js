78f475d874908523f353a936c68fcef221f9d6af
sejaljain123 <sejaljain2043@gmail.com>
2023-08-15 13:39:30 +0530

    initial commit

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.get("/", (req, res) => {
   res.send("First server!!");
