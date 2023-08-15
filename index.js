bf10a5724ce6d82f57c0c276caa966c1819daa20
sejaljain123 <sejaljain2043@gmail.com>
2023-08-15 15:16:41 +0530

    fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 3001;
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
